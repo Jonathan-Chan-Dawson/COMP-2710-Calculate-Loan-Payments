c8fe640913274c6bf6fac38fb776a1fe42226da2
Jonathan <jonafunny6@gmail.com>
2026-09-15 13:06:44 -0500

    Fix formatting in amortization table output for better alignment

--- loan_payment.h
+++ loan_payment.h
@@ -30,11 +30,11 @@ int calculate_months_loan_payment(double loan_amount, double yearly_interest_rat
             << std::setw(10) << "Principal" << "\n";
   std::cout << "---------------------------------------------------------\n";
   std::cout << std::left << std::setw(8) << "0"
-            << std::setw(13) << balance
-            << std::setw(9) << "N/A"
-            << std::setw(9) << "N/A"
-            << std::setw(9) << "N/A"
-            << std::setw(9) << "N/A" << "\n";
+            << std::setw(14) << balance
+            << std::setw(10) << "N/A"
+            << std::setw(10) << "N/A"
+            << std::setw(10) << "N/A"
+            << std::setw(10) << "N/A" << "\n";
 
   while (balance > 0.0)
   {
@@ -45,11 +45,11 @@ int calculate_months_loan_payment(double loan_amount, double yearly_interest_rat
     balance = max(0.0, balance);
 
     std::cout << std::left << std::setw(8) << month
-              << std::setw(13) << balance
-              << std::setw(9) << payment
-              << std::setw(9) << rate
-              << std::setw(9) << interest
-              << std::setw(9) << principal << "\n";
+              << std::setw(14) << balance
+              << std::setw(10) << payment
+              << std::setw(10) << rate
+              << std::setw(10) << interest
+              << std::setw(10) << principal << "\n";
 
     if (principal <= 0.0)
     {
